e50003302461c318b28e48b9b0e858ed817c1c95
pragya2611 <pragyat2611@gmail.com>
2025-04-02 10:10:38 +0530

    refactor: String calculator should return the sum of multiple numbers separated by newline and comma

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -10,9 +10,16 @@ export class StringCalculator implements ICalculator {
         if (numbers.length === 1 && !numbers.includes(',')) { 
             return parseFloat(numbers); 
         }
-        // Split by both comma (`,`) and newline (`\n`)
-        const numArray = numbers.split(/[\n,]/).map(num => parseFloat(num.trim()));
 
+        const delimiters = [',', '\n'];
+
+        // Create a regex pattern dynamically using the delimiters array
+        const delimiterPattern = new RegExp(`[${delimiters.join('')}]`);
+
+        // Split based on the dynamic delimiter pattern
+        const numArray = numbers.split(delimiterPattern).map(num => parseFloat(num.trim()));
+
+       
         return numArray.reduce((sum, num) => sum + num, 0);       
     }
 }
