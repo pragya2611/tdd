db97e8e80ff12c51e16c0c1963671d43621d9fd1
pragya2611 <pragyat2611@gmail.com>
2025-04-02 09:50:00 +0530

    fix: String calculator should return the sum of multiple numbers(2 numbers) separated by newline

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -15,6 +15,11 @@ export class StringCalculator implements ICalculator {
             // Then sum them up
             return numbers.split(',').map(num => parseFloat(num.trim())).reduce((sum, num) => sum + num, 0);
         }
+        if (numbers.includes('\n')) {
+            // Split the string by newline and convert to numbers
+            // Then sum them up
+            return numbers.split('\n').map(num => parseFloat(num.trim())).reduce((sum, num) => sum + num, 0);
+        }
         return 0;
     }
 }
